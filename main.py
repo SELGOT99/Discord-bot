b3402255eaa627e69c87cd17fee2e3fa160d80de
SELGOT99 <selgotkarouk@gmail.com>
2025-10-01 09:57:17 +0100

    Update main.py

--- main.py
+++ main.py
@@ -57,36 +57,7 @@ async def z9dm(ctx):
 
     await ctx.send(f"✅ الإعلان تْصيفط لـ {sent} عضو.\n❌ ما وصلش لـ {failed} عضو (ربما سادين DM).")
 
-@bot.command()
-@commands.has_permissions(administrator=True)
-async def m(ctx):
-    channel_id = 1419796886340108299  # هنا id ديال القناة اللي بغيت تدير ليها tag
-    channel_mention = f"  START ➡️​ <#{1419796886340108299}>"
 
-    embed = discord.Embed(
-        title="ZAN9A ROLEPLAY STATUS",
-        description="",
-        color=0xff0000
-    )
-    embed.add_field(name="METTING  ON ✅ ", value="", inline=False)
-    embed.set_image(url="https://i.postimg.cc/wjbcVM0P/a87.png")
-
-
-    sent = 0
-    failed = 0
-
-    for member in ctx.guild.members:
-        if member.bot:
-            continue
-        try:
-            await member.send(content=f"{channel_mention}", embed=embed)
-            sent += 1
-        except:
-            failed += 1
-
-    await ctx.send(f"✅ الإعلان تْصيفط لـ {sent} عضو.\n❌ ما وصلش لـ {failed} عضو (ربما سادين DM).")
 # ---------- Start ----------
 keep_alive()
-bot.run(os.getenv("TOKEN"))
-
-
+bot.run(os.getenv("TOKEN")) 
